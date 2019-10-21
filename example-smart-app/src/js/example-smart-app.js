b368d563ad7d40bf827791b0b7a63c1fdf09788b
hkhosravian3 <56801915+hkhosravian3@users.noreply.github.com>
2019-10-20 20:26:55 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -15,10 +15,10 @@
                     type: 'Observation',
                     query: {
                       code: {
-                        $or: ['http://loinc.org|29463-7', 'http://loinc.org|8462-4',
+                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4'] 
-                              //'http://loinc.org|8302-2']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
+                              'http://loinc.org|29463-7']
                       }
                     }
                   });
@@ -37,7 +37,7 @@
             lname = patient.name[0].family.join(' ');
           }
 
-          //var height = byCodes('8302-2');
+          var height = byCodes('8302-2');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -49,7 +49,7 @@
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
-          //p.height = getQuantityValueAndUnit(height[0]);
+          p.height = getQuantityValueAndUnit(height[0]);
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -82,7 +82,7 @@
       lname: {value: ''},
       gender: {value: ''},
       birthdate: {value: ''},
-      //height: {value: ''},
+      height: {value: ''},
       systolicbp: {value: ''},
       diastolicbp: {value: ''},
       ldl: {value: ''},
@@ -126,7 +126,7 @@
     $('#lname').html(p.lname);
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
-    //$('#height').html(p.height);
+    $('#height').html(p.height);
     $('#systolicbp').html(p.systolicbp);
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
